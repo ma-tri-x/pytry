d118b8dc10e5165173b80bf6ca19f501aa1f171a
ma-tri-x <niob@gmx.li>
2015-08-23 23:12:00 +0200

    replace.py etwas geändert und laptop version von python_ssh_pi hochgeladen

--- python_ssh_pi.py
+++ python_ssh_pi.py
@@ -6,16 +6,27 @@ import getpass
     #def __init__(self,
 
 class address:
+    ip_prefix = "192.168.1"
     def __init__(self,user,hostip,pswd):
         self.suffix = 0
         self.user = user
-        self.host_ip = "{}{}".format(hostip,self.suffix)
+        ip_prefix = hostip
+        self.host_ip = "{}.{}".format(ip_prefix,self.suffix)
         self.password = pswd
     
     #def check(self):
     def next_ip(self):
         self.suffix+=1
-        self.host_ip = "{}{}".format(hostip,self.suffix)
+        self.host_ip = "{}.{}".format(ip_prefix,self.suffix)
+        
+    def ip(self):
+        return self.host_ip
+        
+    def us(self):
+        return self.user
+        
+    def pwd(self):
+        return self.pwd
         
 
 def main():
@@ -23,14 +34,17 @@ def main():
     hostname = raw_input('host-ip (without last number): ')
     username = raw_input('username: ')
     password = getpass.getpass('password: ')
+    cl = address(username,hostname,password)
     for i in range(256):
         print i,":"
         try:                                                            
-            s.login (hostname, username, password)
+            s.login (cl.us,cl.ip,cl.pwd)
         except pxssh.ExceptionPxssh, e:
             print "pxssh failed on login."
             print str(e)
+            cl.next_ip
             pass
+        print "Login Success"
         s.sendline ('uptime')   # run a command
         s.prompt()             # match the prompt
         print s.before          # print everything before the prompt.
--- replace.py
+++ replace.py
@@ -1,13 +1,24 @@
 import json
 import re
 
-with open('replacedict.json') as data_file:    
-    repl_dict = json.load(data_file)
-    
-with open('somethingtoreplace.txt') as data_file:    
-    str_file = data_file.read()
+def main():
+    with open('replacedict.json') as data_file:    
+        repl_dict = json.load(data_file)
+        
+    with open('somethingtoreplace.txt') as data_file:    
+        str_file = data_file.read()
 
-print repl_dict[0]
-print str_file
+    for item,value in repl_dict.items():
+        print '{} : {}'.format(item,value)
+        searchObj = re.search(str_file,item,flags=0)
+        if searchObj:
+            print "searchObj.group() : ", searchObj.group()
+            print "searchObj.group(1) : ", searchObj.group(1)
+            print "searchObj.group(2) : ", searchObj.group(2)
+    #print str_file
 
-scientific_num = re.compile("^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$")
+    #scientific_num = re.compile("^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$")
+
+
+if __name__ == "__main__":
+    main()
